ebade43df619b526335966e11a34d8839d61c930
mintuchel <max3627@naver.com>
2025-05-23 16:04:04 +0900

    refactor: app.js to use .env PORT var

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import projectRouter from './src/routes/project.js';
+import issueRouter from './src/routes/issue.js';
+import userRouter from './src/routes/user.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT;
 
 const app = express();
-const PORT = 8080;
 
 // middle-ware
 app.use(express.urlencoded({ extended: false }));
 
 // router
-app.use('/projects', projectRouter); // 해당 라우터에 prefix 적용
+// 해당 라우터에 prefix 적용
+app.use('/projects', projectRouter);
+app.use('/issues', issueRouter);
+app.use('/users', userRouter);
 
 // server
 app.listen(PORT, () => {
