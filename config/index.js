c4dcb1ab9bd536fed0b139edc1d78e39de70b5d2
mintuchel <max3627@naver.com>
2025-05-26 11:14:59 +0900

    refactor: configs

---
+++ config/index.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const env = {
+  PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: Number(process.env.DB_PORT),
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  MSG_QUEUE_URL: process.env.MSG_QUEUE_URL,
+  EXCHANGE_NAME: process.env.EXCHANGE_NAME
+}
