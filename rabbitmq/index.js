0ec753c54e787aae3096e05b5a4cba3291701884
mintuchel <max3627@naver.com>
2025-05-30 00:55:58 +0900

    update: getConnection func

--- rabbitmq/index.js
+++ rabbitmq/index.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 let connection;
 
-export async function getConnection() {
+async function getConnection() {
     if (connection) return connection;
 
     connection = await amqp.connect(env.MSG_QUEUE_URL);
