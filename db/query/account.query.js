fe2336e08e774b428473991dc8eae29fb6c10bb8
mintuchel <max3627@naver.com>
2025-06-02 13:42:30 +0900

    update: live queries to mysql procedure calls

--- db/query/account.query.js
+++ db/query/account.query.js
@@ -1,7 +1,7 @@
 export const AccountQuery = {
-    FIND_ALL: 'SELECT uid, email, name FROM account',
-    FIND_BY_ID: 'SELECT * FROM account WHERE uid = ?',
-    SAVE: `INSERT INTO account (uid, email, name, password) VALUES (?, ?, ?, ?)`,
-    UPDATE_BY_ID: `UPDATE account SET email = ?, name = ?, password = ? WHERE uid = ?`,
-    DELETE_BY_ID: `DELETE FROM account WHERE uid = ?`,
+    FIND_ALL: 'CALL MJH_find_all_accounts();',
+    FIND_BY_ID: 'CALL MJH_find_account_by_id(?)',
+    SAVE: `CALL MJH_save_account(?, ?, ?, ?)`,
+    UPDATE_BY_ID: `CALL MJH_update_account_by_id(?, ?, ?, ?)`,
+    DELETE_BY_ID: `CALL MJH_delete_account_by_id(?)`,
 }
