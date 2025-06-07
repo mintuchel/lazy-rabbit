b230b0b37498857a036bdd29bffd2221a76185ed
mintuchel <max3627@naver.com>
2025-06-08 02:53:41 +0900

    add: system folder for debug and logging

---
+++ system/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const logs = require("./logs");
+
+module.exports.info = logs.info;
+module.exports.debug = logs.debug;
+module.exports.error = logs.error;
+module.exports.log = logs.log;
+module.exports.warning = logs.warning;
