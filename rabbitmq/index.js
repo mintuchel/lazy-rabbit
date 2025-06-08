a5312a276be5b91b7203b0b4202f128f85b95d61
mintuchel <max3627@naver.com>
2025-06-09 02:09:16 +0900

    feat: added error logging using event-emmiter's emit on func

--- rabbitmq/index.js
+++ rabbitmq/index.js
@@ -9,17 +9,30 @@ class MessageBroker extends EventEmitter {
     constructor() {
         super();
         this.connection = null;
+        
+        this.on('connected', () => {
+            system.log('[MESSAGE-BROKER] Connected to RabbitMQ');
+        });
+  
+        this.on('error', (err) => {
+            system.error('[MESSAGE-BROKER] Connection error occurred:', err.message);
+        });
     }
 
     async getConnection() {
         if (this.connection) return this.connection;
 
-        this.connection = await amqp.connect(env.MSG_QUEUE_URL);
-        return this.connection;
+        try {
+            this.connection = await amqp.connect(env.MSG_QUEUE_URL);
+            this.emit('connected');
+            return this.connection;
+        } catch (err) {
+            this.emit('error', err);
+        }
     }
 
     async run() {
-        this.getConnection();
+        await this.getConnection();
         system.debug("MessageBroker start");
         setInterval(() => {
             system.debug("MessageBroker is running");
@@ -32,69 +45,79 @@ class MessageBroker extends EventEmitter {
             const channel = await connection.createChannel();
             return channel;
         } catch (err) {
-            throw err;
+            this.emit('error', err);
         }
     };
 
     async sendRpcMessage(channel, queue, requestBody) {
-        // 매 request 마다 할당하기 위한 id 생성
-        const correlationId = uuidv4();
-        // RpcServer 로부터 응답을 받기 위한 익명큐 하나 선언
-        const { queue: replyQueue } = await channel.assertQueue("", { exclusive: true });
-        // 해당 consumer 고유 식별 번호
-        const consumerTag = uuidv4();
+        try {
+            // 매 request 마다 할당하기 위한 id 생성
+            const correlationId = uuidv4();
+            // RpcServer 로부터 응답을 받기 위한 익명큐 하나 선언
+            const { queue: replyQueue } = await channel.assertQueue("", { exclusive: true });
+            // 해당 consumer 고유 식별 번호
+            const consumerTag = uuidv4();
 
-        // 성공/실패에 따라 resolve/reject를 호출할 수 있는 Promise 반환
-        return new Promise(function (resolve, reject) {
-            // replyQueue로 들어온 메시지 받기
-            channel.consume(replyQueue, function (msg) {
+            // 성공/실패에 따라 resolve/reject를 호출할 수 있는 Promise 반환
+            return new Promise(function (resolve, reject) {
+                // replyQueue로 들어온 메시지 받기
+                channel.consume(replyQueue, function (msg) {
 
-                // 내가 보낸 메시지가 맞다면 resolve 처리
-                if (msg.properties.correlationId === correlationId) {
-                    const response = JSON.parse(msg.content.toString());
-                    resolve(response);
-                    // 메시지를 받았다면 cancel 처리?
-                    channel.cancel(consumerTag);
-                }
-            }, {
-                noAck: true,
-                consumerTag,
-            });
+                    // 내가 보낸 메시지가 맞다면 resolve 처리
+                    if (msg.properties.correlationId === correlationId) {
+                        const response = JSON.parse(msg.content.toString());
+                        resolve(response);
+                        // 메시지를 받았다면 cancel 처리?
+                        channel.cancel(consumerTag);
+                    }
+                }, {
+                    noAck: true,
+                    consumerTag,
+                });
 
-            console.log("[SENT] destination queue : %s, msg : %s", queue.name, JSON.stringify(requestBody));
+                console.log("[SENT] destination queue : %s, msg : %s", queue.name, JSON.stringify(requestBody));
     
-            // 특정 queue로 메시지 전송
-            channel.sendToQueue(queue.name, Buffer.from(JSON.stringify(requestBody)), {
-                correlationId,
-                replyTo: replyQueue,
-            });
+                // 특정 queue로 메시지 전송
+                channel.sendToQueue(queue.name, Buffer.from(JSON.stringify(requestBody)), {
+                    correlationId,
+                    replyTo: replyQueue,
+                });
 
-            // 10초 동안 응답이 안오면 cancel 
-            setTimeout(function () {
-                channel.cancel(consumerTag);
-                reject(new Error('RPC timeout'));
-            }, 10000);
-        });
+                // 10초 동안 응답이 안오면 cancel 
+                setTimeout(function () {
+                    channel.cancel(consumerTag);
+                    reject(new Error('RPC timeout'));
+                }, 10000);
+            });
+        } catch (err) {
+            system.error("[MESSAGE-BROKER] RPC (SEND): ", err.message);
+            this.emit('error', err);
+        }
     }
 
     async recieveRpcMessage(channel, queue, onRecieve) {
-        channel.consume(queue.name, async (msg) => {
-            if (!msg) return;
+        try {
+            channel.consume(queue.name, async (msg) => {
+                if (!msg) return;
 
-            const messagePayload = JSON.parse(msg.content.toString());
+                const request = JSON.parse(msg.content.toString());
+                const response = await onRecieve(request);
 
-            try {
-                const responsePayload = await onRecieve(messagePayload);
-
-                // replyTo 큐로 응답 전송
-                channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(responsePayload)), {
-                    correlationId: msg.properties.correlationId
-                });
-            } catch (err) {
-                console.error("Error handling RPC request: ", err);
-            }
-            channel.ack(msg);
-        });
+                try {
+                    // replyTo 큐로 응답 전송
+                    channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(response)), {
+                        correlationId: msg.properties.correlationId
+                    });
+                } catch (err) {
+                    system.error("[MESSAGE-BROKER] RPC (SEND-TO-QUEUE): ", err.message);
+                    this.emit('error', err);
+                }
+                channel.ack(msg);
+            });
+        } catch (err) {
+            system.error("[MESSAGE-BROKER] RPC (RECIEVE): ", err.message);
+            this.emit('error', err);
+        }
     }
 
     // 특정 Exchange로 특정 RoutingKey를 가진 메시지 발행
@@ -103,8 +126,9 @@ class MessageBroker extends EventEmitter {
             await channel.assertExchange(exchange.name, exchange.type, { durable: exchange.durable });
             channel.publish(exchange.name, routingKey, Buffer.from(JSON.stringify(requestBody)));
             console.log("[SENT] destination exchange : %s, routingKey : %s, msg : %s", exchange.name, routingKey, JSON.stringify(requestBody));
-        } catch (error) {
-            system.error("[MESSAGE-BROKER] PUBLISH TO EXCHANGE: ", error);
+        } catch (err) {
+            system.error("[MESSAGE-BROKER] PUBLISH TO EXCHANGE: ", err.message);
+            this.emit('error', err);
         }
     }
 
@@ -127,8 +151,9 @@ class MessageBroker extends EventEmitter {
             },{
                 noAck: false,
             });
-        } catch (error) {
-            system.error("[MESSAGE-BROKER] SUBSCRIBE TO EXCHANGE: ", error);
+        } catch (err) {
+            system.error("[MESSAGE-BROKER] SUBSCRIBE TO EXCHANGE: ", err.message);
+            this.emit('error', err);
         }
     };
 }
