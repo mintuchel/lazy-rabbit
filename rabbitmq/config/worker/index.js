f3671b103e36139dc66972ffab5ab0c260db49dd
mintuchel <max3627@naver.com>
2025-06-22 17:57:27 +0900

    fix: worker configs

--- rabbitmq/config/worker/index.js
+++ rabbitmq/config/worker/index.js
@@ -3,31 +3,31 @@ const QueueDefinitions = require("../queue");
 
 const WorkerDefinitions = {
     AUTH_SERVICE: {
-        name: 'AuthService',
+        name: 'AUTH_SERVICE',
         exchangeDefinition: ExchangeDefinitions.AUTH_EXCHANGE,
         queueDefinition: QueueDefinitions.AUTH_REQUEST_QUEUE,
         bindingKey: 'auth.#'
     },
     SMS_WORKER: {
-        name: 'sms-notification',
+        name: 'SMS_NOTIFICATION_WORKER',
         exchangeDefinition: ExchangeDefinitions.NOTIFICATION_EXCHANGE,
         queueDefinition: QueueDefinitions.NOTIFICATION_SMS_QUEUE,
         bindingKey: 'notification.sms.#'
     },
     EMAIL_WORKER: {
-        name: 'email-notification',
+        name: 'EMAIL_NOTIFICATION_WORKER',
         exchangeDefinition: ExchangeDefinitions.NOTIFICATION_EXCHANGE,
         queueDefinition: QueueDefinitions.NOTIFICATION_EMAIL_QUEUE,
         bindingKey: 'notification.email.#'
     },
     SLACK_WORKER: {
-        name: 'slack-notification',
+        name: 'SLACK_NOTIFICATION_WORKER',
         exchangeDefinition: ExchangeDefinitions.NOTIFICATION_EXCHANGE,
         queueDefinition: QueueDefinitions.NOTIFICATION_SLACK_QUEUE,
         bindingKey: 'notification.slack.#'
     },
     DEAD_LETTER_WORKER: {
-        name: 'dead-letter-worker',
+        name: 'DEAD_LETTER_WORKER',
         exchangeDefinition: ExchangeDefinitions.NOTIFICATION_DLX,
         queueDefinition: QueueDefinitions.NOTIFICATION_DLQ,
         bindingKey: 'notification.#.dlq'
