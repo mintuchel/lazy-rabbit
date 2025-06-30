847f59c9d262d1aa5f82e047a7ea7e33c1dd1ab1
mintuchel <max3627@naver.com>
2025-06-30 09:43:53 +0900

    feat: add lib for message-broker instance in demo app

---
+++ example/lib/message-broker.js
@@ -0,0 +1,7 @@
+const MessageBroker = require("../../lib");
+const { env } = require("../config");
+
+const messageBroker = new MessageBroker(env.MSG_QUEUE_URL);
+
+// export MessageBroker instance used across example demo app
+module.exports = messageBroker;
